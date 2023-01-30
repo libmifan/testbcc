88daa3ff700dcc7440b861f4bf3a521cbd958114
worker02 <worker02@local>
2023-01-30 03:45:08 +0000

    sys_sync.py: version 1

---
+++ sys_sync.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from bcc import BPF
+import sys
+import time
+
+prog ='''
+int kprobe__sys_sync(void *ctx) {
+    bpf_trace_printk("sys_sync() called\\n"); 
+    return 0;
+}
+'''
+
+try:
+    print("Tracing sys_sync()... Ctrl-C to end")
+    time.sleep(2)
+    BPF(text=prog).trace_print()
+except KeyboardInterrupt:
+    sys.exit(3)
+
